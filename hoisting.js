61f9aeb7ad96b7899b1e8c75fb0a4ca43bea0960
sanober riaz <sanober@sanober.me>
2018-01-05 04:16:17 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -12,10 +12,9 @@ function callMe() {
 function crazy() {
   // fix the code in here:
 
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
-    thisIsCrazy();
+    thisIsCrazy()();
   }
 }
 
